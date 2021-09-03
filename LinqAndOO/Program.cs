1ff170c302f412e382b96cd9623c69cef1bd4daa
GET Academy <terje@kolderup.net>
2021-09-03 09:59:37 +0200

    Initial commit

---
+++ LinqAndOO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqAndOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
